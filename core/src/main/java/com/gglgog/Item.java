0ff0612d15dc475d7de939cd51b15368dd943022
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-13 19:18:52 +0200

    Update Item.java

--- core/src/main/java/com/gglgog/Item.java
+++ core/src/main/java/com/gglgog/Item.java
@@ -1,19 +1,37 @@
 package com.gglgog;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Item {
     public String name;
     public String id;
     public String texture;
+    public String spawnText;
     public boolean isCraftable;
     public CraftingRecipe craftRecipe;
     public String model3D;
-    public String spawnText; // текст/число при спавне
 
-    public Item(String name, String id, String texture, boolean isCraftable) {
+    // Галочки
+    public boolean canBreak;      // можно ломать
+    public boolean hasInventory;  // [] Инвентарь
+    public boolean personalInv;   // [] Личный
+
+    // Списки
+    public List<String> dropItems;
+
+    public Item(String name, String id, boolean isCraftable) {
         this.name = name;
         this.id = id;
-        this.texture = texture;
+        this.texture = "";
+        this.spawnText = "1";
         this.isCraftable = isCraftable;
-        this.spawnText = "";
+        this.craftRecipe = null;
+        this.model3D = "";
+
+        this.canBreak = true;
+        this.hasInventory = false;
+        this.personalInv = false;
+        this.dropItems = new ArrayList<>();
     }
 }
