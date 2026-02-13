1c9cd3f7f71812aaf645c118e5d53d624a031f08
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-13 20:49:54 +0200

    Update Main.java

--- core/src/main/java/com/gglgog/Main.java
+++ core/src/main/java/com/gglgog/Main.java
@@ -1,48 +1,23 @@
 package com.gglgog;
 
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) {
 
-        MinecraftMod mod = new MinecraftMod("МойКастомныйМод","GGLGOG","Пример блока с галочками и вариантами","1.26");
+    public static void main(String[]args){
 
-        Block oakBlock = new Block("Oak Planks","oak_planks");
+        MinecraftMod mod=new MinecraftMod("MyMod","GGLGOG","1.26");
 
-        // Настройки галочек
-        oakBlock.isBreakable = true;
-        oakBlock.hasCrafting = true;
-        oakBlock.canRotate = true;
-        oakBlock.personalInventory = true;
-        oakBlock.openInventory = true;
+        for(Block b:ListAll.blocks()){
+            mod.addBlock(b);
+            TextureGenerator.gen(b.id);
+        }
 
-        // Варианты разрушения
-        oakBlock.destroySpeedMode = "custom";
-        oakBlock.customDestroySpeed = 2.5;
+        for(Item i:ListAll.items()){
+            mod.addItem(i);
+            TextureGenerator.gen(i.id);
+        }
 
-        // Крафт 3x3
-        String[][] grid = {
-            {"stick","stick","stick"},
-            {"oak_planks","oak_planks","oak_planks"},
-            {"stick","stick","stick"}
-        };
-        boolean[][] enabled = {
-            {true,true,true},
-            {true,true,true},
-            {true,true,true}
-        };
-        oakBlock.setCrafting(grid, enabled);
+        ModExporter.export(mod);
 
-        // Инвентарь блока
-        oakBlock.inventory.addItem("stick");
-
-        // Добавляем блок в мод
-        mod.addBlock(oakBlock);
-
-        // Экспорт мода
-        ModExporter exporter = new ModExporter("./output");
-        exporter.exportMod(mod);
-
-        System.out.println("Мод с галочками и крафтом создан!");
+        System.out.println("DONE!");
     }
 }
